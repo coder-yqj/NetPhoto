30d8ae91997eae38eb89be1699e598627df71c0f
lovelyCoder <yqj_lovely@163.com>
2016-05-08 14:14:17 +0800

    aaa

--- src/com/bishe/photo/dao/VoteDao.java
+++ src/com/bishe/photo/dao/VoteDao.java
@@ -1,7 +1,8 @@
 package com.bishe.photo.dao;
 
+import com.bishe.photo.annotation.MyAnnotation;
 import com.bishe.photo.entity.Vote;
-
+@MyAnnotation
 public interface VoteDao {
 	
 	public void saveVote(Vote vote);
